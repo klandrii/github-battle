c8108215c61518b49c4d54c828893b244c64b0eb
Andrii <demonictoast@gmail.com>
2017-02-23 16:01:14 +0200

    React Container Components vs Presentational Components
    redo presentational component as a stateless fucntion

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
     contextTypes: {
@@ -10,12 +10,12 @@ var PromptContainer = React.createClass({
             username: ''
         }
     },
-    onUpdateUser: function (e) {
+    handleUpdateUser: function (e) {
         this.setState({
             username: e.target.value
         })
     },
-    onSubmitUser: function (e) {
+    handleSubmitUser: function (e) {
         e.preventDefault();
         var username = this.state.username;
         this.setState({
@@ -37,28 +37,11 @@ var PromptContainer = React.createClass({
     render: function () {
         
         return(
-        <div className = "jumbotron col-sm-6 col-sm-offset text-center" style ={transparentBg}>
-            <h1>{this.props.route.header}</h1>
-                <div className ="col-sm-12" >
-                    <form onSubmit={this.onSubmitUser}>
-                        <div className = "form-group">
-                            <input 
-                                className = "form-control"
-                                placeholder="Github Username"
-                                onChange = {this.onUpdateUser}
-                                value = {this.state.username}
-                                type="text"/>
-                        </div>
-                        <div className = "form-group col-sm-4 col-sm-offset-4">
-                            <button 
-                                className = "btn btn-block btn-success"
-                                type = "submit">
-                                    Continue
-                                </button>
-                        </div>
-                    </form>
-                </div>
-        </div>
+            <Prompt 
+                onSubmitUser={this.handleSubmitUser}
+                onUpdateUser={this.handleUpdateUser}
+                header = {this.props.route.header}
+                username={this.state.username}/>
         )}
 });
 
