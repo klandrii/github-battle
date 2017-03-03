ac1730bf565d7adb871c5d1f74a826e2db53fd1e
Andrii <demonictoast@gmail.com>
2017-03-03 17:28:35 +0200

    Added Semantic-UI-React
    you can see the loader after entering both players

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,43 +1,53 @@
+var Loader = require('semantic-ui-react').Loader;
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
-var Link =  require('react-router').Link;
+var Link = require('react-router').Link;
 var UserDetails = require('../components/UserDetails');
 var UserDetailsWrapper = require('../components/UserDetailsWrapper');
 
-function puke (object) {
+function puke(object) {
     return <pre>{JSON.stringify(object, null, ' ')}</pre>
 }
 
-function ConfirmBattle (props) {
+
+
+function ConfirmBattle(props) {
     return props.isLoading === true
-    ? <p> LOADING! </p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-        <h1>Confirm Players</h1>
-        <div className='col-sm-8 col-sm-offset-2' >
-            <UserDetailsWrapper header = 'Player One' >
-                <UserDetails info= {props.playersInfo[0]} />
-            </UserDetailsWrapper>
-            <UserDetailsWrapper header = 'Player Two'>
-                <UserDetails info= {props.playersInfo[1]} />
-            </UserDetailsWrapper>
-        </div>
-        <div className='btn group '>
-            <div className='col-sm-12' >
-                <button type='button'className='btn btn-lg btn-success  ' onClick={props.onInitiateBattle} >
-                    Initiate Battle!
-                </button>
-                <Link to='/playerOne' style={styles.space}>
-                    <button type='button'className='btn btn-lg btn-danger  ' onClick={props.onInitiateBattle} >
-                    Reselect Players
+        ? <p>
+            <Loader active inline='centered' content='Loading' />
+        </p>
+        : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+            <h1>Confirm Players</h1>
+            <div className='col-sm-8 col-sm-offset-2'>
+                <UserDetailsWrapper header='Player One'>
+                    <UserDetails info={props.playersInfo[0]}/>
+                </UserDetailsWrapper>
+                <UserDetailsWrapper header='Player Two'>
+                    <UserDetails info={props.playersInfo[1]}/>
+                </UserDetailsWrapper>
+            </div>
+            <div className='btn group '>
+                <div className='col-sm-12'>
+                    <button
+                        type='button'
+                        className='btn btn-lg btn-success  '
+                        onClick={props.onInitiateBattle}>
+                        Initiate Battle!
                     </button>
-                </Link>
+                    <Link to='/playerOne' style={styles.space}>
+                        <button
+                            type='button'
+                            className='btn btn-lg btn-danger  '
+                            onClick={props.onInitiateBattle}>
+                            Reselect Players
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
-    </div>
 
 }
-
 
 ConfirmBattle.propTypes = {
     isLoading: PropTypes.bool.isRequired,
